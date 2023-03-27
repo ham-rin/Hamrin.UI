aa7c431496aa9a73874398cc41fd94270d59e70a
Aref Azizian <storm.aref@gmail.com>
2023-03-27 19:12:32 +0330

    request location permission

--- lib/presentation/controllers/home_controller.dart
+++ lib/presentation/controllers/home_controller.dart
@@ -1,5 +1,13 @@
+import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 
-class HomeController extends GetxController{
+class HomeController extends GetxController {
+  var isLocationPermissionOk = true.obs;
 
+  requestLocationPermission() async {
+    LocationPermission permission = await Geolocator.requestPermission();
+    isLocationPermissionOk.value =
+        permission == LocationPermission.whileInUse ||
+            permission == LocationPermission.always;
+  }
 }
--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hamrin_app/presentation/controllers/home_controller.dart';
 
 class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  final HomeController controller = Get.put(HomeController());
+
   @override
   Widget build(context) {
+    controller.requestLocationPermission();
     return const Scaffold(body: Center(child: Text("Home")));
   }
 }
